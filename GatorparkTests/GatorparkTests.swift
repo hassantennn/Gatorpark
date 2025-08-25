91c1b843d4bef930942d9c5a551f1a88dd4b3dc9
Muhammad Hassan <158739616+hassantennn@users.noreply.github.com>
2025-08-25 15:10:30 +0100

    Initial Commit

---
+++ GatorparkTests/GatorparkTests.swift
@@ -0,0 +1,17 @@
+//
+//  GatorparkTests.swift
+//  GatorparkTests
+//
+//  Created by APPLE on 25/08/2025.
+//
+
+import Testing
+@testable import Gatorpark
+
+struct GatorparkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
